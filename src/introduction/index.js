068040e6fc34bff4c231affef3eeb8982cfad5a1
sunyongjian <704928775@qq.com>
2017-10-03 00:04:24 +0800

    feat: sort :blush

--- src/introduction/index.js
+++ src/introduction/index.js
@@ -1,6 +1,6 @@
-const ary = [-1, -5, -33, -22, 0, 2, 3, 22];
+const ary = [-1, -5, -33, -22, -1, 0, 2, 3, 22];
 console.log(ary.sort());
-// [ -1, -22, -33, -5, 0, 2, 22, 3 ]
+// [ -1, -1, -22, -33, -5, 0, 2, 22, 3 ]
 // ary.sort，当 sort 中不传入比较器的时候，默认是按照字符编码的顺序排序，即第一个数字的顺序。
 
 // 升序
@@ -12,11 +12,23 @@ const compare = (x, y) => {
 
 console.log(ary.sort(compare));
 
-// [ -33, -22, -5, -1, 0, 2, 3, 22 ]
+// [ -33, -22, -5, -1, -1, 0, 2, 3, 22 ]
 
 // but，这个 compare 函数不通用于比较，比如
 if (compare(1, 1)) {
   console.log('equal');
 }
-// 因为返回值是 0.
 
+const thanOrEqual = (x, y) => x >= y;
+
+if (thanOrEqual(1, 1)) {
+  console.log('thanOrEqual');
+}
+
+const compartor = behavior => (x, y) => {
+  if (behavior(x, y)) return 1;
+  return -1;
+};
+
+console.log(ary.sort(compartor(thanOrEqual)));
+//  [ -33, -22, -5, -1, -1, 0, 2, 3, 22 ]
