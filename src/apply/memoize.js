be52101948c0ca8b8521fb6fab811dd8d3a47cd3
sunyongjian <704928775@qq.com>
2018-01-28 23:58:25 +0800

    add

--- src/apply/memoize.js
+++ src/apply/memoize.js
@@ -32,3 +32,17 @@ console.timeEnd('capitalized');
 
 // 第一次大写的处理可能需要 几毫米，有了缓存后，之后几乎是 0.025ms，节省很多的性能。
 // 不过这个只是一个普通的栗子，单个参数是没问题，如果是多个参数呢.
+
+let fibonacci = (n) => {
+  return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+console.time('fibonacci');
+console.log(fibonacci(20));
+console.timeEnd('fibonacci');
+
+// fibonacci = memoize(fibonacci);
+
+// console.time('memoizeFib');
+// console.log(fibonacci(20));
+// console.timeEnd('memoizeFib');
